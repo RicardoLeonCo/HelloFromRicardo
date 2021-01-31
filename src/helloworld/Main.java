f5935bcbca8c45e2f989f405930f03b46e7283cc
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-01-30 19:00:52 -0800

    added my greeting

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello my name is Ricardo.");
+	System.out.println("Hello Ricardo, my name is Jandy.");
     }
 }
